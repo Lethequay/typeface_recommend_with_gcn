dc89543654989702fc35693f2dceedda32fea335
yongqyu <yongqyu@gmail.com>
2018-06-28 21:09:21 +0900

    preprocess for dataloader

---
+++ data_loader.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import random
+from random import shuffle
+import numpy as np
+import torch
+from torch.utils import data
+from torchvision import transforms
+from PIL import Image, ImageOps, ImageFont, ImageDraw
+
+class ImageFolder(data.Dataset):
+	"""Load Variaty Chinese Fonts for Iterator. """
+	def __init__(self, data_path, image_path, transform=None):
+		"""Initializes image paths and preprocessing module."""
+		self.data_arr = np.load(data_path)
+		self.data_size = len(self.data_arr)
+		self.image_path = image_path
+		self.image_paths = list(map(lambda x: os.path.join(image_path, x), os.listdir(image_path)))
+		self.transform = transform
+		print("data count :", self.data_size)
+		print("image count :", len(self.image_paths))
+
+	def __getitem__(self, index):
+		"""Reads an image from a file and preprocesses it and returns."""
+		tokens = self.data_arr[index]
+		typography = tokens[0]
+		text = tokens[1:]
+
+		image = Image.open(self.image_path+
+						   typography.replace(' ','_').replace('/','-')+'.png')
+		image = image.resize((440,231), Image.ANTIALIAS)
+		if self.transform is not None:
+			image = self.transform(image)
+		print(image.size())
+
+		return typography, image, text
+
+	def __len__(self):
+		"""Returns the total number of font files."""
+		return self.data_size
+
+
+def get_loader(data_path, image_path, batch_size, num_workers=2):
+	"""Builds and returns Dataloader."""
+
+	transform = transforms.Compose([
+					transforms.ToTensor(),
+					transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+	dataset = ImageFolder(data_path, image_path, transform)
+	data_loader = data.DataLoader(dataset=dataset,
+								  batch_size=batch_size,
+								  shuffle=True,
+								  num_workers=num_workers)
+	return data_loader
+
---
+++ main.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+from solver import Solver
+from data_loader import get_loader
+from torch.backends import cudnn
+
+
+def main(config):
+	cudnn.benchmark = True
+
+	data_loader = get_loader(data_path=config.data_path,
+							 image_path=config.img_path,
+							 batch_size=config.batch_size,
+							 num_workers=config.num_workers)
+
+	# Create directories if not exist
+	if not os.path.exists(config.model_path):
+		os.makedirs(config.model_path)
+	if not os.path.exists(config.sample_path):
+		os.makedirs(config.sample_path)
+	if not os.path.exists(config.result_path):
+		os.makedirs(config.result_path)
+
+	# Train and sample the images
+	solver = Solver(config, data_loader)
+	solver.train()
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+
+	# model hyper-parameters
+	parser.add_argument('--image_size', type=int, default=128)
+	parser.add_argument('--z_dim', type=int, default=256)
+
+	# training hyper-parameters
+	parser.add_argument('--sample_epochs', type=int, default=100)
+	parser.add_argument('--num_epochs', type=int, default=200)
+	parser.add_argument('--num_epochs_decay', type=int, default=100)
+	parser.add_argument('--batch_size', type=int, default=4)
+	parser.add_argument('--num_workers', type=int, default=8)
+	parser.add_argument('--lr', type=float, default=0.0002)
+	parser.add_argument('--lambda_cls', type=float, default=1)
+	parser.add_argument('--beta1', type=float, default=0.5)		# momentum1 in Adam
+	parser.add_argument('--beta2', type=float, default=0.999)	  # momentum2 in Adam
+
+	# misc
+	parser.add_argument('--mode', type=str, default='train')
+	parser.add_argument('--model_path', type=str, default='./models')
+	parser.add_argument('--sample_path', type=str, default='./samples')
+	parser.add_argument('--result_path', type=str, default='./results')
+	parser.add_argument('--data_path', type=str, default='./data/fiu_indexed.npy')
+	parser.add_argument('--img_path', type=str, default='./data/png/')
+	parser.add_argument('--log_step', type=int , default=3000)
+	parser.add_argument('--val_step', type=int , default=5)
+
+	config = parser.parse_args()
+	print(config)
+	main(config)
+
